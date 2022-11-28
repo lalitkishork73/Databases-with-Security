0af4fcfbaae64add756927d771b3e10448634836
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-11-29 00:43:08 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { default: mongoose } = require("mongoose");
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("connected 3000")
+})
+
+
